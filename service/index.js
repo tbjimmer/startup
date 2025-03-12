68744c81de71e6c95637a230315bd53b5db99e59
tbjimmer <tbjim04@byu.edu>
2025-03-12 13:24:07 -0600

    undo

--- service/index.js
+++ service/index.js
@@ -2,3 +2,12 @@ const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
 app.use(express.static('public'));
 
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  server: {
+    proxy: {
+      '/api': 'http://localhost:4000',
+    },
+  },
+});
